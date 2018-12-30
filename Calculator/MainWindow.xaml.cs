95bfd56653c93872ac6749654c346be3a890a552
Jakub Gucwa <jakub.gucwa@outlook.com>
2018-12-30 21:20:58 +0100

    event handlers

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -20,9 +20,61 @@ namespace Calculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber, result;
         public MainWindow()
         {
             InitializeComponent();
+
+            resultLabel.Content = "123";
+
+            //event handler 
+            acButton.Click += AcButtonOnClick;
+            negativeButton.Click += NegativeButtonOnClick;
+            percentageButton.Click += PercentageButtonOnClick;
+            equalsButton.Click += EqualsButtonOnClick;
+        }
+
+        private void EqualsButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void PercentageButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber /100;
+                resultLabel.Content = lastNumber.ToString();
+
+            }
+        }
+
+        private void NegativeButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(resultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber = lastNumber * -1;
+                resultLabel.Content = lastNumber.ToString();
+                
+            }
+        }
+
+        private void AcButtonOnClick(object sender, RoutedEventArgs e)
+        {
+            resultLabel.Content = "0";
+        }
+
+        private void SevenButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (resultLabel.Content.ToString() == "0")
+            {
+                resultLabel.Content = "7";
+            }
+            else
+            {
+                resultLabel.Content = $"{resultLabel.Content}7";
+            }
+
         }
     }
 }
