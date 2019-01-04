1f451d8e71cb9e4012d5d5aa5df3a6dd7933f97e
Jakub Gucwa <jakub.gucwa@outlook.com>
2019-01-04 20:52:32 +0100

    message box xaml look - za pomoca static resurce i podstawowy styling

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -220,6 +220,12 @@ namespace Calculator
         }
         public static double Divide(double n1, double n2)
         {
+            if (n2 ==0)
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK,
+                    MessageBoxImage.Error);
+                return 0;
+            }
             return n1 / n2;
         }
     }
